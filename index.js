5fbd03ec097fee783208d2a781821a0a558a3577
Павлова Тамара Махмудовна <ptm@college.spk>
2021-05-19 16:35:01 +0700

    layout Toma

---
+++ index.js
@@ -0,0 +1,23 @@
+let wrapper = document.querySelector('.wrapper');
+console.log(wrapper);
+
+let screen = document.querySelector('.screen');
+console.log(screen);
+
+let btn = document.querySelector('.btn');
+console.log(btn);
+
+let allBtn = document.querySelectorAll('.info__btn');
+console.log(allBtn);
+
+let info__btn_z = document.querySelector('.info__btn_z');
+console.log(info__btn_z);
+
+let info__btn_m = document.querySelector('.info__btn_m');
+console.log(info__btn_m);
+
+let info__btn_p = document.querySelector('.info__btn_p');
+console.log(info__btn_p);
+
+let info__btn_r = document.querySelector('.info__btn_r');
+console.log(info__btn_r);
